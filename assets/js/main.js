58f381824829e837ffc08f5dc01c46d70dbfb6e2
andreivinicius <andreivini31@gmail.com>
2025-05-06 17:12:43 -0300

    Portifolio Andrei Vinicius

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+const menu = document.querySelector('header')
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.bloco__inicio', { duration: 1500 });
+
+sr.reveal('.bloco__sobre', { 
+    rotate: { x:0, y:80, z:0},
+    duration: 3000 
+});
+
+sr.reveal('.bloco__projetos', { 
+    rotate: { x:80, y:0, z:0},
+    duration: 3000 });
+
+sr.reveal('.bloco__habilidades', { duration: 1500 });
+
+sr.reveal('.bloco__contato', { duration: 1000 });
+
+sr.reveal('.flashcard', { 
+    delay: 1000,
+    duration: 2000
+});
+
+function activeScroll() {
+    menu.classList.toggle('ativo', scrollY > 10);
+}
+
+window.addEventListener('scroll', activeScroll);
+
+// slider animation
+
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 10000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById('radio'+count).checked = true;
+
+
+}
